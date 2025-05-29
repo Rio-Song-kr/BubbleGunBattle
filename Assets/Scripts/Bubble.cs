239845c7f781a986cde7314b0f1e7e24f99458b4
Rio_Song <rio.dw.song@gmail.com>
2025-05-29 15:18:50 +0900

    [Feat] Shoot 시 Bubble Shader가 적용된 Bubble 발사 구현 * Bubble 발사 구현 * 일정 시간 후 터지는 효과 구현

--- Assets/Scripts/Bubble.cs
+++ Assets/Scripts/Bubble.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Bubble : MonoBehaviour
 {
-    [SerializeField] private float _force;
+    [SerializeField] private BubbleDataSO _bubbleData;
 
     private Rigidbody _rigidbody;
 
@@ -15,16 +15,48 @@ public class Bubble : MonoBehaviour
 
     private void OnEnable()
     {
-        Destroy(gameObject, 5f);
+        transform.localScale = Vector3.one * _bubbleData.StartScale;
         Move();
     }
 
-    private void FixedUpdate()
+    private void Start()
     {
+        StartCoroutine(GrowBubble());
+        Invoke(nameof(DestroyBubble), _bubbleData.DestroyDelay);
     }
 
     private void Move()
     {
-        _rigidbody.AddForce(transform.forward * _force, ForceMode.Impulse);
+        _rigidbody.AddForce(transform.forward * _bubbleData.Force, ForceMode.Impulse);
+    }
+
+    private IEnumerator GrowBubble()
+    {
+        float time = 0f;
+
+        while (time < _bubbleData.GrowDuration)
+        {
+            float scale = Mathf.Lerp(_bubbleData.StartScale, _bubbleData.EndScale, time / _bubbleData.GrowDuration);
+            transform.localScale = Vector3.one * scale;
+
+            time += Time.deltaTime;
+
+            yield return null;
+        }
+
+        transform.localScale = Vector3.one * _bubbleData.EndScale;
+    }
+
+    private void DestroyBubble()
+    {
+        //# 추후 Object Pooling 적용 시 수정될 예정
+        var effect = Instantiate(_bubbleData.PopEffect, transform.position, transform.rotation);
+        Destroy(effect, 1f);
+        Destroy(gameObject);
+    }
+
+    private void CancelDestroy()
+    {
+        CancelInvoke(nameof(DestroyBubble));
     }
 }
