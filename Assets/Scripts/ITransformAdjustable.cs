2c6713d811b9daceff9720b6e725daad0fffc047
Rio_Song <rio.dw.song@gmail.com>
2025-05-30 11:46:45 +0900

    [Fix] 경사로에서 Player 포획 시 포지션 변화 관련 bug fix * Player의 Center와 Bubble의 Center가 일치하지 않아, bubble이 회전할 때, player도 같이 회전하면서 발생한 문제

--- Assets/Scripts/ITransformAdjustable.cs
+++ Assets/Scripts/ITransformAdjustable.cs
@@ -4,6 +4,8 @@ public interface ITransformAdjustable
 {
     public void SetPosition(Vector3 position);
     public Vector3 GetPosition();
+    public void SetLocalPosition(Vector3 position);
+    public Vector3 GetLocalPosition();
     public void SetLocalScale(Vector3 localScale);
     public void SetParent(Transform parent, bool worldPositionStays = true);
 }
